93d05732d7c7dcd7de4746cbc54eb58293f226f5
YuryKoropenko <yurykoropenko@gmail.com>
2017-07-04 16:54:26 +0300

    commit 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,71 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	$('.h-nav a[href="#block-price"]').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+				var $target = $(this.hash);
+				$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+				if ($target.length) {
+					var targetOffset = $target.offset().top;
+					$('html,body').animate({scrollTop: targetOffset}, 500);//скорость прокрутки
+					return false;
+				}
+			}
+	});
+	$('.h-nav a[href="#block-marsh"]').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+				var $target = $(this.hash);
+				$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+				if ($target.length) {
+					var targetOffset = $target.offset().top;
+					$('html,body').animate({scrollTop: targetOffset}, 500);//скорость прокрутки
+					return false;
+				}
+			}
+	});
+	$('.f-nav a[href="#block-price"]').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+				var $target = $(this.hash);
+				$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+				if ($target.length) {
+					var targetOffset = $target.offset().top;
+					$('html,body').animate({scrollTop: targetOffset}, 500);//скорость прокрутки
+					return false;
+				}
+			}
+	});
+	$('.f-nav a[href="#block-marsh"]').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+				var $target = $(this.hash);
+				$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+				if ($target.length) {
+					var targetOffset = $target.offset().top;
+					$('html,body').animate({scrollTop: targetOffset}, 500);//скорость прокрутки
+					return false;
+				}
+			}
+	});
+
+	/*form*/
+	$('.h-nav__button input').on('click', function() {
+		$('.popup-bg').fadeIn();
+		$('.popup-reservation').fadeIn();
+	});
+	$('.p-priceList__button button').on('click', function() {
+		$('.popup-bg').fadeIn();
+		$('.popup-reservation').fadeIn();
+	});
+
+	$('.popup-bg').on('click', function() {
+		$(this).fadeOut();
+		$('.popup-reservation').fadeOut();
+	});
+	$('.popup-reservation__close button').on('click', function() {
+		$('.popup-bg').fadeOut();
+		$('.popup-reservation').fadeOut();
+	});
+
+
+});
